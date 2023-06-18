203d16986462fd91952fc06eef2ed08a9cf95b10
Yunative <47847960+yi7242@users.noreply.github.com>
2023-06-18 22:28:02 +0900

    Update UserScript.user.js

--- UserScript.user.js
+++ UserScript.user.js
@@ -1,28 +1,21 @@
 // ==UserScript==
 // @name         Add link to Problems
-// @namespace    http://tampermonkey.net/
-// @version      0.1
+// @namespace    https://atcoder.jp/
+// @version      0.2
 // @description  Add links to AtCoder Problems Userpage on AtCoder profile.
 // @author       You
 // @match        https://atcoder.jp/users/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=atcoder.jp
-// @grant        none
+// @exclude      https://atcoder.jp/users/?/history
+// @exclude      https://atcoder.jp/users/?/history/*
 // ==/UserScript==
 
 (function() {
     'use strict';
-
-    // Your code here...
     let dlTables = document.getElementsByClassName('dl-table');
     let tbody = dlTables[0].getElementsByTagName('tbody')[0];
-    
     let userName = document.getElementsByClassName('username')[0].getElementsByTagName('span')[0].textContent;
     let link = "https://kenkoooo.com/atcoder/#/user/" + userName;
     let insertPoint = document.getElementsByClassName("col-md-3 col-sm-12")[0].getElementsByTagName('h3')[0];
-    
     let newTr = '<tr><th class="no-break">Problems</th><td><a href=' +link+' target="_blank" style="">' + userName + '</a></td></tr>'
     tbody.insertAdjacentHTML("beforeend", newTr)
-
-
-
 })();
